4eebd2bbe51d31ca3b2ea17fd0dda044002ec66c
chriz63 <chmenzel567@gmail.com>
2023-07-06 19:50:38 +0200

    FEATURE: inital commit

---
+++ TemperatureDisplay.ino
@@ -0,0 +1,11 @@
+// Project TemperatureDisplay based on NodeMCU with DHT22 Sensor and SH1106 128X64 Display
+
+// Initializing all that will be need
+void setup() {
+    
+}
+
+// Run the program 
+void loop() {
+
+}
